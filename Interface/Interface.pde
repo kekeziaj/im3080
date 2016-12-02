2f3a03c2c95ed75922ae4030d3b9c09a2467dd2b
Denny Basillie <denny077@hotmail.com>
2016-12-03 02:20:05 +0800

    Updated piano code and included change music feature for beat mode. Version 2

--- Interface/Interface.pde
+++ Interface/Interface.pde
@@ -1,3 +1,6 @@
+//Written by Denny
+//U1420246C
+
 import processing.sound.*;
 import processing.serial.*;
 import javax.swing.JFileChooser;
@@ -9,31 +12,40 @@ import java.nio.file.Paths;
 
 import java.awt.event.KeyEvent;
 
-Serial port;  
+Serial port;  //arduino serial port
 SoundFile sample;
 Amplitude rms;
 File selectedFile;
 
 int val = -1;      // Data received from the serial port
-int prevVal = -1;
+int prevVal = -1; //to detect mode change
+
+int currTime; //to detect end of music
 
 // Used for smoothing
 float sum;
 float scale=5;
 float smooth_factor=0.25;
 
-boolean[] isPressed = new boolean[13];
+float x,y,btnWidth,btnHeight; //layout size for "Change Music" button in beat mode
 
+boolean[] isPressed = new boolean[13]; //which key is pressed in piano mode
 
 public void setup() {
-    size(200,200);
+    size(640,480);
     for (int i = 0; i < isPressed.length; i++ ) {
       isPressed[i] = false;
     }
     while (Serial.list().length == 0) {
 
     }
-    String portName = Serial.list()[0];
+    
+          
+    x = width * 4/5.0;
+    btnWidth = width / 5.5;
+    y =  height * 7/8.0;
+    btnHeight = height / 10;
+    String portName = Serial.list()[0]; //get arduino port
     port = new Serial(this, portName, 57600); 
 }      
 
@@ -42,28 +54,22 @@ public void draw() {
     background(0,0,0);
     stroke(0,0,0);
     fill(255,255,255);
+    textSize(height/40);
   
-    if (port.available() > 0) {
-      prevVal = val;
+    if (port.available() > 0) { //new mode requested
+      prevVal = val; //store previous mode code
       val = port.read();
     }
     
-    if (val == 1) {  //Sound analyze
-      if (prevVal != 1) {
-        JFileChooser fileChooser = new JFileChooser();
-        JFrame frame = new JFrame();
-        fileChooser.setCurrentDirectory(new File("C:\\Users\\DennyBasillie\\Downloads\\Compressed\\processing-3.2.1\\BeatWrite\\data"));
-        int result = fileChooser.showOpenDialog(frame);
-        selectedFile = fileChooser.getSelectedFile();
+    if (val == 1) {  //Beat analyze
+      if (prevVal != 1) { 
+        chooseFile(); //initialize mode
         
-        while (result != JFileChooser.APPROVE_OPTION || (!selectedFile.getAbsolutePath().endsWith(".mp3") && !selectedFile.getAbsolutePath().endsWith(".wav"))) {
-          result = fileChooser.showOpenDialog(frame);
-          selectedFile = fileChooser.getSelectedFile();
-        }
-
-        //Load and play a soundfile and loop it
+        
+        //Load and play a soundfile
         sample = new SoundFile(this, selectedFile.getAbsolutePath());
-        sample.loop();
+        sample.play();
+        currTime = millis(); //get elapsed time 
         
         // Create and patch the rms tracker
         rms = new Amplitude(this);
@@ -71,21 +77,41 @@ public void draw() {
         prevVal = 1;
       }
       
-      // smooth the rms data by smoothing factor
-      sum += (rms.analyze() - sum) * smooth_factor;  
+      //end of initialization
+      
+      if ((millis() - currTime) > sample.duration()*1000) {
+        sum = 0;
+        sample.play();
+        currTime = millis();
+        rms.input(sample);
+      }
+      
+      sum += (rms.analyze() - sum) * smooth_factor;        // smooth the rms data by smoothing factor
 
-      // rms.analyze() return a value between 0 and 1. It's
-      // scaled to height/2 and then multiplied by a scale factor
-      float rms_scaled=sum*(height/2)*scale;
-      System.out.println(rms_scaled);
-      port.write(Math.round(rms_scaled));
+      float rms_scaled=sum*(height/2)*scale;       // rms.analyze() return a value between 0 and 1. It's scaled to height/2 and then multiplied by a scale factor
+      float arduino_scaled = sum*500;
+      System.out.println(arduino_scaled); //DEBUG purpose
+      
+      if (arduino_scaled > 0.01) {  //prune insignificant amplitude
+              port.write(Math.round(arduino_scaled));
+      }
 
       // We draw an ellispe coupled to the audio analysis
+      fill(255, 255, 255);
       ellipse(width/2, height/2, rms_scaled, rms_scaled);
+
+      //cursor within "Change Music" box
+      if (inRegion(x,y,btnWidth,btnHeight)) {
+        fill(128,128,128);
+      }
+      rect(x, y, btnWidth , btnHeight);
+      fill(0,0,0);
+      text("Change Music", width * 4.15/5.0, height * 7.5/8.0); 
+
     }
     
     if (val == 2) { //piano
-        for (int index = 0; index < 8; ++index) {
+        for (int index = 0; index < 8; ++index) { //initialize layout 
           if (isPressed[index]) {
              fill(128,128,128);
           } else {
@@ -112,9 +138,52 @@ public void draw() {
         }
       }
     }
+    
+public boolean chooseFile() {
+  JFileChooser fileChooser = new JFileChooser(); //prompt music selection
+  JFrame frame = new JFrame();
+  fileChooser.setCurrentDirectory(new File("C:\\Users\\DennyBasillie\\Downloads\\Compressed\\processing-3.2.1\\BeatWrite\\data"));
+  int result = fileChooser.showOpenDialog(frame);
+  selectedFile = fileChooser.getSelectedFile();
+  
+  if (prevVal != 1) { //start of mode (not due to "Change Music")
+    while (result != JFileChooser.APPROVE_OPTION || (!selectedFile.getAbsolutePath().endsWith(".mp3") && !selectedFile.getAbsolutePath().endsWith(".wav"))) {
+      result = fileChooser.showOpenDialog(frame);
+      selectedFile = fileChooser.getSelectedFile();
+    }
+  } else {
+    while (result == JFileChooser.APPROVE_OPTION && !selectedFile.getAbsolutePath().endsWith(".mp3") && !selectedFile.getAbsolutePath().endsWith(".wav")) {
+      result = fileChooser.showOpenDialog(frame);
+      selectedFile = fileChooser.getSelectedFile();
+    }
+    
+    return (result == JFileChooser.APPROVE_OPTION);
+  }
+  return true;
+}
+
+public void mousePressed() {
+  if (val == 1 && inRegion(x,y,btnWidth,btnHeight)) { //only applicable to beat mode and click is on the "Change Music" button
+    if (chooseFile()) { //change is valid
+      //reinitialize
+      sample.stop();
+      sum = 0;
+      sample = new SoundFile(this, selectedFile.getAbsolutePath());
+      sample.play();
+      currTime = millis(); //get elapsed time 
+      rms.input(sample);
+    }
+  }
+}
+
+public boolean inRegion (float x, float y, float width, float height) { //cursor is in this region (rectangle)
+ return (mouseX >= x && mouseX <= x + width && 
+      mouseY >= y && mouseY <= y + height);
+        
+}
 
 public void keyPressed() {
-  if (val == 2) {
+  if (val == 2) { //only applicable to piano mode
       switch (key) {
         case ('S'): 
         case ('s'): if (!isPressed[0]) {
@@ -122,6 +191,8 @@ public void keyPressed() {
                         SoundFile note = new SoundFile(this, dataPath("") + "\\c3.ogg");
                         note.play();
                         isPressed[0] = true;
+                        port.write('s'); 
+                        
                       } catch (Exception ex) {
                         System.out.println(ex.toString());
                       }
@@ -134,6 +205,7 @@ public void keyPressed() {
                         SoundFile note = new SoundFile(this, dataPath("") + "\\d3.ogg");
                         note.play();
                         isPressed[1] = true;
+                        port.write('d'); 
                       } catch (Exception ex) {
                         System.out.println(ex.toString());
                       }
@@ -145,6 +217,7 @@ public void keyPressed() {
                         SoundFile note = new SoundFile(this, dataPath("") + "\\e3.ogg");
                         note.play();
                         isPressed[2] = true;
+                        port.write('f'); 
                       } catch (Exception ex) {
                         System.out.println(ex.toString());
                       }
@@ -156,6 +229,7 @@ public void keyPressed() {
                         SoundFile note = new SoundFile(this, dataPath("") + "\\f3.ogg");
                         note.play();
                         isPressed[3] = true;
+                        port.write('g'); 
                       } catch (Exception ex) {
                         System.out.println(ex.toString());
                       }
@@ -167,6 +241,7 @@ public void keyPressed() {
                         SoundFile note = new SoundFile(this, dataPath("") + "\\g3.ogg");
                         note.play();
                         isPressed[4] = true;
+                        port.write('h'); 
                       } catch (Exception ex) {
                         System.out.println(ex.toString());
                       }
@@ -178,6 +253,7 @@ public void keyPressed() {
                         SoundFile note = new SoundFile(this, dataPath("") + "\\a3.ogg");
                         note.play();
                         isPressed[5] = true;
+                        port.write('j'); 
                       } catch (Exception ex) {
                         System.out.println(ex.toString());
                       }
@@ -189,6 +265,7 @@ public void keyPressed() {
                         SoundFile note = new SoundFile(this, dataPath("") + "\\b3.ogg");
                         note.play();
                         isPressed[6] = true;
+                        port.write('k'); 
                       } catch (Exception ex) {
                         System.out.println(ex.toString());
                       }
@@ -200,6 +277,7 @@ public void keyPressed() {
                         SoundFile note = new SoundFile(this, dataPath("") + "\\c4.ogg");
                         note.play();
                         isPressed[7] = true;
+                        port.write('l'); 
                       } catch (Exception ex) {
                         System.out.println(ex.toString());
                       }
@@ -211,6 +289,7 @@ public void keyPressed() {
                         SoundFile note = new SoundFile(this, dataPath("") + "\\c3m.ogg");
                         note.play();
                         isPressed[8] = true;
+                        port.write('e'); 
                       } catch (Exception ex) {
                         System.out.println(ex.toString());
                       }
@@ -222,6 +301,7 @@ public void keyPressed() {
                         SoundFile note = new SoundFile(this, dataPath("") + "\\d3m.ogg");
                         note.play();
                         isPressed[9] = true;
+                        port.write('r'); 
                       } catch (Exception ex) {
                         System.out.println(ex.toString());
                       }
@@ -233,6 +313,7 @@ public void keyPressed() {
                         SoundFile note = new SoundFile(this, dataPath("") + "\\f3m.ogg");
                         note.play();
                         isPressed[10] = true;
+                        port.write('y'); 
                       } catch (Exception ex) {
                         System.out.println(ex.toString());
                       }
@@ -244,6 +325,7 @@ public void keyPressed() {
                         SoundFile note = new SoundFile(this, dataPath("") + "\\g3.ogg");
                         note.play();
                         isPressed[11] = true;
+                        port.write('u'); 
                       } catch (Exception ex) {
                         System.out.println(ex.toString());
                       }
@@ -255,6 +337,7 @@ public void keyPressed() {
                         SoundFile note = new SoundFile(this, dataPath("") + "\\a3m.ogg");
                         note.play();
                         isPressed[12] = true;
+                        port.write('i'); 
                       } catch (Exception ex) {
                         System.out.println(ex.toString());
                       }
@@ -265,7 +348,7 @@ public void keyPressed() {
 }
   
 public void keyReleased() {
-    if (val == 2) {
+    if (val == 2) { //only applicable to piano mode
       switch (key) {
         case ('S'): 
         case ('s'): isPressed[0] = false;
--- Interface/application.windows64/source/Interface.java
+++ Interface/application.windows64/source/Interface.java
@@ -24,6 +24,9 @@ import java.io.IOException;
 
 public class Interface extends PApplet {
 
+//Written by Denny
+//U1420246C
+
 
 
 
@@ -35,21 +38,24 @@ public class Interface extends PApplet {
 
 
 
-Serial port;  
+Serial port;  //arduino serial port
 SoundFile sample;
 Amplitude rms;
 File selectedFile;
 
 int val = -1;      // Data received from the serial port
-int prevVal = -1;
+int prevVal = -1; //to detect mode change
+
+int currTime; //to detect end of music
 
 // Used for smoothing
 float sum;
 float scale=5;
 float smooth_factor=0.25f;
 
-boolean[] isPressed = new boolean[13];
+float x,y,btnWidth,btnHeight; //layout size for "Change Music" button in beat mode
 
+boolean[] isPressed = new boolean[13]; //which key is pressed in piano mode
 
 public void setup() {
     
@@ -59,7 +65,13 @@ public void setup() {
     while (Serial.list().length == 0) {
 
     }
-    String portName = Serial.list()[0];
+    
+          
+    x = width * 4/5.0f;
+    btnWidth = width / 5.5f;
+    y =  height * 7/8.0f;
+    btnHeight = height / 10;
+    String portName = Serial.list()[0]; //get arduino port
     port = new Serial(this, portName, 57600); 
 }      
 
@@ -68,28 +80,22 @@ public void draw() {
     background(0,0,0);
     stroke(0,0,0);
     fill(255,255,255);
+    textSize(height/40);
   
-    if (port.available() > 0) {
-      prevVal = val;
+    if (port.available() > 0) { //new mode requested
+      prevVal = val; //store previous mode code
       val = port.read();
     }
     
-    if (val == 1) {  //Sound analyze
-      if (prevVal != 1) {
-        JFileChooser fileChooser = new JFileChooser();
-        JFrame frame = new JFrame();
-        fileChooser.setCurrentDirectory(new File("C:\\Users\\DennyBasillie\\Downloads\\Compressed\\processing-3.2.1\\BeatWrite\\data"));
-        int result = fileChooser.showOpenDialog(frame);
-        selectedFile = fileChooser.getSelectedFile();
+    if (val == 1) {  //Beat analyze
+      if (prevVal != 1) { 
+        chooseFile(); //initialize mode
         
-        while (result != JFileChooser.APPROVE_OPTION || (!selectedFile.getAbsolutePath().endsWith(".mp3") && !selectedFile.getAbsolutePath().endsWith(".wav"))) {
-          result = fileChooser.showOpenDialog(frame);
-          selectedFile = fileChooser.getSelectedFile();
-        }
-
-        //Load and play a soundfile and loop it
+        
+        //Load and play a soundfile
         sample = new SoundFile(this, selectedFile.getAbsolutePath());
-        sample.loop();
+        sample.play();
+        currTime = millis(); //get elapsed time 
         
         // Create and patch the rms tracker
         rms = new Amplitude(this);
@@ -97,21 +103,41 @@ public void draw() {
         prevVal = 1;
       }
       
-      // smooth the rms data by smoothing factor
-      sum += (rms.analyze() - sum) * smooth_factor;  
+      //end of initialization
+      
+      if ((millis() - currTime) > sample.duration()*1000) {
+        sum = 0;
+        sample.play();
+        currTime = millis();
+        rms.input(sample);
+      }
+      
+      sum += (rms.analyze() - sum) * smooth_factor;        // smooth the rms data by smoothing factor
 
-      // rms.analyze() return a value between 0 and 1. It's
-      // scaled to height/2 and then multiplied by a scale factor
-      float rms_scaled=sum*(height/2)*scale;
-      System.out.println(rms_scaled);
-      port.write(Math.round(rms_scaled));
+      float rms_scaled=sum*(height/2)*scale;       // rms.analyze() return a value between 0 and 1. It's scaled to height/2 and then multiplied by a scale factor
+      float arduino_scaled = sum*500;
+      System.out.println(arduino_scaled); //DEBUG purpose
+      
+      if (arduino_scaled > 0.01f) {  //prune insignificant amplitude
+              port.write(Math.round(arduino_scaled));
+      }
 
       // We draw an ellispe coupled to the audio analysis
+      fill(255, 255, 255);
       ellipse(width/2, height/2, rms_scaled, rms_scaled);
+
+      //cursor within "Change Music" box
+      if (inRegion(x,y,btnWidth,btnHeight)) {
+        fill(128,128,128);
+      }
+      rect(x, y, btnWidth , btnHeight);
+      fill(0,0,0);
+      text("Change Music", width * 4.15f/5.0f, height * 7.5f/8.0f); 
+
     }
     
     if (val == 2) { //piano
-        for (int index = 0; index < 8; ++index) {
+        for (int index = 0; index < 8; ++index) { //initialize layout 
           if (isPressed[index]) {
              fill(128,128,128);
           } else {
@@ -138,9 +164,52 @@ public void draw() {
         }
       }
     }
+    
+public boolean chooseFile() {
+  JFileChooser fileChooser = new JFileChooser(); //prompt music selection
+  JFrame frame = new JFrame();
+  fileChooser.setCurrentDirectory(new File("C:\\Users\\DennyBasillie\\Downloads\\Compressed\\processing-3.2.1\\BeatWrite\\data"));
+  int result = fileChooser.showOpenDialog(frame);
+  selectedFile = fileChooser.getSelectedFile();
+  
+  if (prevVal != 1) { //start of mode (not due to "Change Music")
+    while (result != JFileChooser.APPROVE_OPTION || (!selectedFile.getAbsolutePath().endsWith(".mp3") && !selectedFile.getAbsolutePath().endsWith(".wav"))) {
+      result = fileChooser.showOpenDialog(frame);
+      selectedFile = fileChooser.getSelectedFile();
+    }
+  } else {
+    while (result == JFileChooser.APPROVE_OPTION && !selectedFile.getAbsolutePath().endsWith(".mp3") && !selectedFile.getAbsolutePath().endsWith(".wav")) {
+      result = fileChooser.showOpenDialog(frame);
+      selectedFile = fileChooser.getSelectedFile();
+    }
+    
+    return (result == JFileChooser.APPROVE_OPTION);
+  }
+  return true;
+}
+
+public void mousePressed() {
+  if (val == 1 && inRegion(x,y,btnWidth,btnHeight)) { //only applicable to beat mode and click is on the "Change Music" button
+    if (chooseFile()) { //change is valid
+      //reinitialize
+      sample.stop();
+      sum = 0;
+      sample = new SoundFile(this, selectedFile.getAbsolutePath());
+      sample.play();
+      currTime = millis(); //get elapsed time 
+      rms.input(sample);
+    }
+  }
+}
+
+public boolean inRegion (float x, float y, float width, float height) { //cursor is in this region (rectangle)
+ return (mouseX >= x && mouseX <= x + width && 
+      mouseY >= y && mouseY <= y + height);
+        
+}
 
 public void keyPressed() {
-  if (val == 2) {
+  if (val == 2) { //only applicable to piano mode
       switch (key) {
         case ('S'): 
         case ('s'): if (!isPressed[0]) {
@@ -148,6 +217,8 @@ public void keyPressed() {
                         SoundFile note = new SoundFile(this, dataPath("") + "\\c3.ogg");
                         note.play();
                         isPressed[0] = true;
+                        port.write('s'); 
+                        
                       } catch (Exception ex) {
                         System.out.println(ex.toString());
                       }
@@ -160,6 +231,7 @@ public void keyPressed() {
                         SoundFile note = new SoundFile(this, dataPath("") + "\\d3.ogg");
                         note.play();
                         isPressed[1] = true;
+                        port.write('d'); 
                       } catch (Exception ex) {
                         System.out.println(ex.toString());
                       }
@@ -171,6 +243,7 @@ public void keyPressed() {
                         SoundFile note = new SoundFile(this, dataPath("") + "\\e3.ogg");
                         note.play();
                         isPressed[2] = true;
+                        port.write('f'); 
                       } catch (Exception ex) {
                         System.out.println(ex.toString());
                       }
@@ -182,6 +255,7 @@ public void keyPressed() {
                         SoundFile note = new SoundFile(this, dataPath("") + "\\f3.ogg");
                         note.play();
                         isPressed[3] = true;
+                        port.write('g'); 
                       } catch (Exception ex) {
                         System.out.println(ex.toString());
                       }
@@ -193,6 +267,7 @@ public void keyPressed() {
                         SoundFile note = new SoundFile(this, dataPath("") + "\\g3.ogg");
                         note.play();
                         isPressed[4] = true;
+                        port.write('h'); 
                       } catch (Exception ex) {
                         System.out.println(ex.toString());
                       }
@@ -204,6 +279,7 @@ public void keyPressed() {
                         SoundFile note = new SoundFile(this, dataPath("") + "\\a3.ogg");
                         note.play();
                         isPressed[5] = true;
+                        port.write('j'); 
                       } catch (Exception ex) {
                         System.out.println(ex.toString());
                       }
@@ -215,6 +291,7 @@ public void keyPressed() {
                         SoundFile note = new SoundFile(this, dataPath("") + "\\b3.ogg");
                         note.play();
                         isPressed[6] = true;
+                        port.write('k'); 
                       } catch (Exception ex) {
                         System.out.println(ex.toString());
                       }
@@ -226,6 +303,7 @@ public void keyPressed() {
                         SoundFile note = new SoundFile(this, dataPath("") + "\\c4.ogg");
                         note.play();
                         isPressed[7] = true;
+                        port.write('l'); 
                       } catch (Exception ex) {
                         System.out.println(ex.toString());
                       }
@@ -237,6 +315,7 @@ public void keyPressed() {
                         SoundFile note = new SoundFile(this, dataPath("") + "\\c3m.ogg");
                         note.play();
                         isPressed[8] = true;
+                        port.write('e'); 
                       } catch (Exception ex) {
                         System.out.println(ex.toString());
                       }
@@ -248,6 +327,7 @@ public void keyPressed() {
                         SoundFile note = new SoundFile(this, dataPath("") + "\\d3m.ogg");
                         note.play();
                         isPressed[9] = true;
+                        port.write('r'); 
                       } catch (Exception ex) {
                         System.out.println(ex.toString());
                       }
@@ -259,6 +339,7 @@ public void keyPressed() {
                         SoundFile note = new SoundFile(this, dataPath("") + "\\f3m.ogg");
                         note.play();
                         isPressed[10] = true;
+                        port.write('y'); 
                       } catch (Exception ex) {
                         System.out.println(ex.toString());
                       }
@@ -270,6 +351,7 @@ public void keyPressed() {
                         SoundFile note = new SoundFile(this, dataPath("") + "\\g3.ogg");
                         note.play();
                         isPressed[11] = true;
+                        port.write('u'); 
                       } catch (Exception ex) {
                         System.out.println(ex.toString());
                       }
@@ -281,6 +363,7 @@ public void keyPressed() {
                         SoundFile note = new SoundFile(this, dataPath("") + "\\a3m.ogg");
                         note.play();
                         isPressed[12] = true;
+                        port.write('i'); 
                       } catch (Exception ex) {
                         System.out.println(ex.toString());
                       }
@@ -291,7 +374,7 @@ public void keyPressed() {
 }
   
 public void keyReleased() {
-    if (val == 2) {
+    if (val == 2) { //only applicable to piano mode
       switch (key) {
         case ('S'): 
         case ('s'): isPressed[0] = false;
@@ -335,7 +418,7 @@ public void keyReleased() {
       }
   }
 }
-  public void settings() {  size(200,200); }
+  public void settings() {  size(640,480); }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "Interface" };
     if (passedArgs != null) {
