15fe3ab94fda3b89e7dd373175081902b0e2660a
Denny Basillie <denny077@hotmail.com>
2016-12-03 03:36:32 +0800

    Changed explosive beat detection to inline type. Going to make some changes to allow gradual dim (Not solid red). Version 2

--- LEDPattern/testSerial.ino
+++ LEDPattern/testSerial.ino
@@ -4,24 +4,43 @@
 #define NUM_LEDS_PER_STRIP 60
 CRGB leds[NUM_STRIPS][NUM_LEDS_PER_STRIP];
 
-
-boolean once = true;
 int val = 0;
-int prevVal = 0;
-int color = 0;
+int cutoffIndex = 0;
+int prevCutoffIndex = 0;
 
 void setup() {
   // Open serial communications and wait for port to open:
   Serial.begin(57600);
-    FastLED.addLeds<NEOPIXEL, 10>(leds[0], NUM_LEDS_PER_STRIP);
+  FastLED.addLeds<NEOPIXEL, 10>(leds[0], NUM_LEDS_PER_STRIP);  
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
   Serial.write(1);
 }
 
-void loop() { // run over and over
+void loop() { 
   if (Serial.available()) {
+    val = Serial.read();
+    prevCutoffIndex = cutoffIndex;
+    cutoffIndex = 60 - 600.0/val; //obtained from the formula (60-i)/60 * val < 10
+    if (cutoffIndex > 59) cutoffIndex = 59; //prevent index out of range
+    if (cutoffIndex < 0) cutoffIndex = 0;
+  }
+
+  if (cutoffIndex > prevCutoffIndex) { //grow right
+      for(int i = prevCutoffIndex+1; i <= cutoffIndex; i++) {
+        leds[0][i] = CRGB(255,0,0);
+    }
+    
+  } else { //shrink left
+     for(int i = prevCutoffIndex; i > cutoffIndex; i--) {
+        leds[0][i] = CRGB::Black;
+     }
+  }
+  FastLED.show();
+
+  /************************************* EXPLOSIVE BEAT DETECTION 
+    if (Serial.available()) {
     val = Serial.read();
   }
 
@@ -36,5 +55,6 @@ void loop() { // run over and over
       leds[0][i] = CRGB::Black;
     }
               FastLED.show();
+   *********************************************************/
 }
 
